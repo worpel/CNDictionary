4bbc5e5f6a8f75dd1b5fd664376354ffe57e4569
Matt <mattk.gt@gmail.com>
2018-07-03 14:34:45 +0100

    Added dictionary, animations/visuals and search functionality.

---
+++ README.md
@@ -0,0 +1,7 @@
+# CNDictionary
+* A technical dictionary for CN that allows users to search for words.
+## Future improvements/additions:
+* Upgrade to ES6 functions and other code refactoring
+* Implementing React.js to autonomously generate word cards
+* A page featuring the entire dictionary of cards (4 per page) with side scrolling
+* Display multiple word cards that all fit the criteria of the search
---
+++ js/script.js
@@ -0,0 +1,81 @@
+const db = [
+  {
+    title: 'Computational Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Animal Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Human Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Plant Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Fire Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Water Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Stone Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  },
+  {
+    title: 'Ice Thinking',
+    def:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, esse et consequatur vero accusamus optio.'
+  }
+];
+console.log(db.length);
+const searchbox = document.getElementById('searchInput');
+let arrayItem = [Math.floor(db.length * Math.random())];
+console.log(arrayItem);
+
+$(document).ready(function() {
+  $('#container').css('display', 'none');
+  $('#container').fadeIn(2000);
+  let title = document.getElementById('title');
+  let body = document.getElementById('body');
+  let textTypewriter = new Typewriter(title, {});
+  let bodyTypewriter = new Typewriter(body, {
+    typingSpeed: 20
+  });
+  setTimeout(function() {
+    textTypewriter
+      .typeString(db[arrayItem].title)
+      .pauseFor(10000)
+      .start();
+  }, 1200);
+  setTimeout(function() {
+    bodyTypewriter
+      .typeString(db[arrayItem].def)
+      .pauseFor(10000)
+      .start();
+  }, 2600);
+
+  const search = event => {
+    let match = db.filter(element => {
+      return element.title
+        .toLowerCase()
+        .includes(searchbox.value.toLowerCase());
+    });
+    title.innerHTML = match[0].title;
+    body.innerHTML = match[0].def;
+  };
+
+  searchbox.addEventListener('input', search);
+});
